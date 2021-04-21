3b303049328a161050834c58d196dd0585cf2757
Michael Galloy <mgalloy@gmail.com>
2021-04-20 20:27:46 -0600

    Adding epoch file access

---
+++ chromag/epochs/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""Handle properties of the spectral lines."""
+
+import os
+
+import epochs
+
+
+EPOCHS_ROOT = os.path.dirname(os.path.abspath(__file__))
+EPOCHS_CFG = os.path.join(EPOCHS_ROOT, 'epochs.cfg')
+EPOCHS_SPEC = os.path.join(EPOCHS_ROOT, 'epochs.spec.cfg')
+
+
+ep = epochs.EpochConfigParser(EPOCHS_SPEC)
+ep.read(EPOCHS_CFG)
+
+
+def get(property_name, date):
+    return(ep.get(property_name, date))
