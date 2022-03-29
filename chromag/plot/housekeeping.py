9afe78fbb35102a2caf018123e5bb6a2e940ef30
Michael Galloy <mgalloy@gmail.com>
2022-03-29 15:28:34 -0600

    Format x-axis tick values

--- chromag/plot/housekeeping.py
+++ chromag/plot/housekeeping.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import configparser
+import math
 import os
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -14,6 +16,21 @@ HOUSEKEEPING_CFG = os.path.join(PLOT_ROOT, "housekeeping.cfg")
 
 class InvalidPlotType(Exception):
     pass
+
+
+def secs2time(secs, pos):
+    hrs = math.floor(secs / 60.0 / 60.0)
+    mins = math.floor((secs - hrs * 60 * 60) / 60.0)
+    return(f"{hrs:02d}:{mins:02d}")
+
+
+def get_formatter(formatter_name):
+    if formatter_name == "secs2time":
+        formatter = matplotlib.ticker.FuncFormatter(secs2time)
+    else:
+        formatter = None
+
+    return(formatter)
 
 
 def plot_time_series(plot_filename, date, section, df):
@@ -44,18 +61,30 @@ def plot_time_series(plot_filename, date, section, df):
 
     fontsize = 6
 
-    fig, ax = plt.subplots(nrows=1, ncols=1, constrained_layout=True)
+    fig, ax = plt.subplots(nrows=1, ncols=1)#, constrained_layout=True)
     fig.set_size_inches(xsize_inches, ysize_inches)
+
     ax.plot(xvalues, yvalues)
+
     if title is not None:
         ax.set_title(title, fontdict={"fontsize": 8})
+
+    xtickformat = section.get("xtickformat", fallback=None)
+    if xtickformat is not None:
+        formatter = get_formatter(xtickformat)
+        ax.xaxis.set_major_formatter(formatter)
+
     ax.xaxis.set_tick_params(labelsize=fontsize)
     ax.yaxis.set_tick_params(labelsize=fontsize)
+
     ax.set_ylim(bottom=bottom, top=top)
+
     if xtitle is not None:
         ax.set_xlabel(xtitle, fontsize=fontsize)
     if ytitle is not None:
         ax.set_ylabel(ytitle, fontsize=fontsize)
+
+    fig.tight_layout()
     plt.savefig(plot_filename, dpi=dpi)
 
 
