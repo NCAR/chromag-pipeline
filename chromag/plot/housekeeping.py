12b446bd3b5dbb58e37f9b072397daa30f6a9102
Michael Galloy <mgalloy@gmail.com>
2022-03-29 14:56:44 -0600

    Change housekeeping config file to go by plot, not by column name

--- chromag/plot/housekeeping.py
+++ chromag/plot/housekeeping.py
@@ -12,29 +12,72 @@ PLOT_ROOT = os.path.dirname(os.path.abspath(__file__))
 HOUSEKEEPING_CFG = os.path.join(PLOT_ROOT, "housekeeping.cfg")
 
 
-def plot_time_series(plot_filename, column_name, df, options):
-    bottom = options.getfloat("min", fallback=None)
-    top    = options.getfloat("max", fallback=None)
-    xtitle = options.get("xtitle", fallback=None)
-    ytitle = options.get("ytitle", fallback=None)
-    yvalues = df[column_name].values
-    xvalues_name = options.get("xvalues", fallback=None)
+class InvalidPlotType(Exception):
+    pass
 
+
+def plot_time_series(plot_filename, date, section, df):
+    bottom = section.getfloat("min", fallback=None)
+    top    = section.getfloat("max", fallback=None)
+
+    xtitle = section.get("xtitle", fallback=None)
+    ytitle = section.get("ytitle", fallback=None)
+
+    yvalues_name = section.get("yvalues", fallback=None)
+    yvalues = df[yvalues_name].values
+
+    xvalues_name = section.get("xvalues", fallback=None)
     if xvalues_name is None:
         xvalues = np.arange(len(yvalues))
     else:
         xvalues = df[xvalues_name].values
 
-    print(f"plot '{column_name}' to {plot_filename} from {bottom} to {top}")
+    title = section.get("title", fallback=None)
 
-    fig, ax = plt.subplots(nrows=1, ncols=1)
+    xsize = section.getfloat("xsize", fallback="800")
+    ysize = section.getfloat("ysize", fallback="300")
+    dpi = 150
+    xsize_inches = xsize / dpi
+    ysize_inches = ysize / dpi
+
+    print(f"plotting time series '{yvalues_name}' to {plot_filename}")
+
+    fontsize = 6
+
+    fig, ax = plt.subplots(nrows=1, ncols=1, constrained_layout=True)
+    fig.set_size_inches(xsize_inches, ysize_inches)
     ax.plot(xvalues, yvalues)
+    if title is not None:
+        ax.set_title(title, fontdict={"fontsize": 8})
+    ax.xaxis.set_tick_params(labelsize=fontsize)
+    ax.yaxis.set_tick_params(labelsize=fontsize)
     ax.set_ylim(bottom=bottom, top=top)
     if xtitle is not None:
-        ax.set_xlabel(xtitle)
+        ax.set_xlabel(xtitle, fontsize=fontsize)
     if ytitle is not None:
-        ax.set_ylabel(ytitle)
-    plt.savefig(plot_filename)
+        ax.set_ylabel(ytitle, fontsize=fontsize)
+    plt.savefig(plot_filename, dpi=dpi)
+
+
+def plot_scatter(plot_filename, date, section, df):
+    pass
+
+
+def dispatch_plot(date:str, section, df):
+    if "filename" not in section: return
+
+    type = section.get("type", fallback="timeseries")
+    type = type.lower()
+
+    plot_filename = section.get("filename")
+    plot_filename = plot_filename.format(date=date)
+
+    if type == "timeseries":
+        plot_time_series(plot_filename, date, section, df)
+    elif type == "scatter":
+        plot_scatter(plot_filename, date, section, df)
+    else:
+        raise InvalidPlotType(f"invalid plot type '{type}' in '{section.name}' plot")
 
 
 def plot(date:str, housekeeping_filename:str):
@@ -44,13 +87,11 @@ def plot(date:str, housekeeping_filename:str):
     options.read(HOUSEKEEPING_CFG)
 
     df = pd.read_csv(housekeeping_filename, skiprows=6)
-    for column_name in df:
-        if "filename" not in options[column_name]: continue
-
-        plot_filename = options.get(column_name, "filename")
-        plot_filename = plot_filename.format(date=date)
-
-        plot_time_series(plot_filename, column_name, df, options[column_name])
+    for plot_name in options.sections():
+        try:
+            dispatch_plot(date, options[plot_name], df)
+        except InvalidPlotType as e:
+            print(e)
 
 
 if __name__ == "__main__":
