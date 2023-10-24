ede421bcd7181bc0ad52aa70a469907cf639390d
Michael Galloy <mgalloy@gmail.com>
2023-10-24 15:51:13 -0600

    Starting to add tests for #7

---
+++ tests/test_string_helpers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for `chromag` package.
+"""
+
+import pytest
+
+from chromag.string_helpers import truncate
+
+
+def test_truncate_basic():
+    assert truncate("A long test string", 10) == "A long ..."
+    assert truncate("A short string", 20) == "A short string"
+
+
+def test_truncate_padding():
+    assert truncate("A short string", 20, padding=True) == "A short string      "
+
+
+def test_truncate_alternate():
+    assert truncate("A long test string", 10, continuation_chars="..") == "A long t.."
