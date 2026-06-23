2e4fe3540b48469520423d49495c17cf436ee195
Michael Galloy <mgalloy@gmail.com>
2026-06-23 17:36:31 -0600

    Add RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -17,15 +17,15 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 import chromag
 
 # -- Project information -----------------------------------------------------
 
-project = 'chromag'
-copyright = '2023, ChroMag team'
-author = 'ChroMag team'
+project = "chromag"
+copyright = "2023, ChroMag team"
+author = "ChroMag team"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,29 +33,34 @@ author = 'ChroMag team'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 
-htmlhelp_basename = 'epochsdoc'
+htmlhelp_basename = "epochsdoc"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
