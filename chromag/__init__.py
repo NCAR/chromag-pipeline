95de06c9c6a9491b631dd26c05f0a718ece9ab36
Michael Galloy <mgalloy@gmail.com>
2026-08-10 14:38:49 -0600

    Find git revision

--- chromag/__init__.py
+++ chromag/__init__.py
@@ -3,5 +3,27 @@
 """Top-level package for ChroMag parser."""
 
 import importlib.metadata
+import os
+import subprocess
+
+# find version and git revision
 
 __version__ = importlib.metadata.version("chromag")
+
+repo_dir = os.path.dirname(__file__)
+__revision__ = (
+    subprocess.check_output(["git", "-C", repo_dir, "rev-parse", "--short", "HEAD"])
+    .decode("ascii")
+    .strip()
+)
+# add a "*" to the revision if there are uncommitted changes
+p = subprocess.run(["git", "-C", repo_dir, "diff-index", "--quiet", "HEAD", "--"])
+if p.returncode != 0:
+    __revision__ += "*"
+
+description = (
+    subprocess.check_output(["git", "-C", repo_dir, "describe"]).decode("ascii").strip()
+)
+
+if description != f"v{__version__}":
+    __version__ = f"{__version__}-dev"
