8b99e9a08bd3bfbdfcaea4b2f51347d006cee266
Michael Galloy <mgalloy@gmail.com>
2026-08-14 17:13:24 -0600

    Create basic IQUV image displays

--- chromag/display/level1.py
+++ chromag/display/level1.py
@@ -46,8 +46,8 @@ def write_intensity_image(l1_file: ChroMagL1File):
 
     fontsize = 18
     left_pad = 15.0
-    start_height = 60.0
-    line_height = 40.0
+    line_height = 2.0 * fontsize
+    start_height = 2.0 * line_height
     im = ax.imshow(
         imdata,
         vmin=display_min**display_exp,
@@ -105,7 +105,78 @@ def write_iquv_image(l1_file: ChroMagL1File):
         logger.info("creating level1 directory")
         create_dir(l1_dir)
 
-    # [TODO]: write IQUV quicklook
+    state_name = ["I", "Q", "U", "V"]
+    display_name = {"I": "i", "Q": "qu", "U": "qu", "V": "v"}
+
+    ionization = property(l1_file.wave_region, "ionization")
+
+    reduce_factor = 4
+    dpi = 100.0
+    px = 1.0 / dpi
+
+    nx = l1_file.data.shape[2]
+    ny = l1_file.data.shape[1]
+
+    # [TODO]: mask
+
+    fontsize = 10
+    left_pad = 15.0 * reduce_factor
+    line_height = 2.0 * fontsize * reduce_factor
+
+    fig = plt.figure(frameon=False)
+    fig.set_size_inches(2 * nx * px / reduce_factor, 2 * ny * px / reduce_factor)
+
+    for p in range(l1_file.data.shape[0]):
+        sname = state_name[p]
+
+        ax = plt.Axes(fig, [0.5 * (p % 2), 0.5 - 0.5 * (p // 2), 0.5, 0.5])
+        ax.set_axis_off()
+        fig.add_axes(ax)
+
+        dname = display_name[sname]
+        display_min = property(l1_file.wave_region, f"display_{dname}_min")
+        display_max = property(l1_file.wave_region, f"display_{dname}_max")
+        display_exp = property(l1_file.wave_region, f"display_{dname}_exp")
+        display_gamma = property(l1_file.wave_region, f"display_{dname}_gamma")
+        colormap = property(l1_file.wave_region, "colormap")
+
+        imdata = l1_file.data[p, :, :].squeeze()
+        imdata = np.clip(imdata, a_min=display_min, a_max=display_max) ** display_exp
+
+        im = ax.imshow(
+            imdata,
+            vmin=display_min**display_exp,
+            vmax=display_max**display_exp,
+            cmap=colormap,
+            aspect="auto",
+        )
+        if p == 0:
+            ax.text(left_pad, line_height, "MLSO ChroMag", color="w", fontsize=fontsize)
+        if abs(display_exp - 1.0) < 0.001:
+            state_text = sname
+            scaling_text = (
+                f"min/max: ${display_min}-{display_max}$, gamma: {display_gamma:0.1f}"
+            )
+        else:
+            state_text = rf"$\mathrm{{{sname}}}^{{{display_exp}}}$"
+            scaling_text = f"min/max: ${display_min}^{{{display_exp}}}-{display_max}^{{{display_exp}}}$, gamma: {display_gamma:0.1f}"
+        ax.text(
+            left_pad,
+            ny - 2 * line_height,
+            state_text,
+            color="w",
+            fontsize=fontsize,
+        )
+        ax.text(
+            left_pad,
+            ny - line_height,
+            scaling_text,
+            color="w",
+            fontsize=fontsize,
+        )
+
+    plt.savefig(output_filename, dpi=dpi)
+    plt.close(fig)
 
     output_basename = os.path.basename(output_filename)
     logger.info(f"wrote {output_basename}...")
