47205a35c77ddcba11d52b233f521c4ea759e706
Michael Galloy <mgalloy@gmail.com>
2026-08-16 10:43:10 -0600

    Add routine to clear results from an observing day

--- chromag/eod/clearday.py
+++ chromag/eod/clearday.py
@@ -2,12 +2,27 @@
 
 """Module for clearing existing published ChroMag data."""
 
+import os
+import shutil
+
+from ..config import get_basedir
+from ..database import clearday as db_clearday
 from ..logging import logger
+from ..pipeline import step
 
 
+@step()
 def clearday(run):
+    """Remove all results for a day, from the database, process directory, and
+    the web archive (but not the standard archive).
+    """
+    logger.info(f"clearing {run.observing_day}...")
+
+    process_basedir = get_basedir(run.observing_day, "process")
+    process_dir = os.path.join(process_basedir, run.observing_day)
+    logger.info(f"removing process directory {run.observing_day}/...")
+    shutil.rmtree(process_dir)
+
     # [TODO]: implement
-    #  - clear processing directory
     #  - clear database tables
     #  - clear web directories
-    logger.info(f"clearing {run.observing_day}...")
