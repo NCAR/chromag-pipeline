1027e18fa9d72476b8de27a2b7d4efd6843b65ea
Michael Galloy <mgalloy@gmail.com>
2026-08-16 10:38:45 -0600

    Archive level 1 data

--- chromag/archive/level1.py
+++ chromag/archive/level1.py
@@ -1,19 +1,55 @@
 # -*- coding: utf-8 -*-
 
-"""Routines for archiving ChroMag level 1 data, i.e., sending data to cold
-storage.
+"""Routines for archiving ChroMag level 1 data, i.e., tarring and sending level
+1 data to cold storage.
 """
 
+import os
+
 from .. import __version__
-from ..config import get_option
+from ..config import get_option, get_basedir
+from ..file import create_dir, make_tarball, make_tarlist
 from ..logging import logger
 
 
 def archive_l1(run):
-    gateway_dir = get_option("archive", "gateway_dir")
-    tarball_basename = f"{run.observing_day}.chromag.l1.{__version__}.tar.gz"
+    """Create level 1 archive tarball from the files in the level1/ directory."""
+    process_basedir = get_basedir(run.observing_day, "process")
+    l1_dir = os.path.join(process_basedir, run.observing_day, "level1")
+    if not os.path.isdir(l1_dir):
+        logger.warn("no level1/ directory to archive")
+        return
 
-    # [TODO]: make tarball, tarlist
+    tarball_basename = f"{run.observing_day}.chromag.l1.{__version__}.tar.gz"
+    tarball_filename = os.path.join(l1_dir, tarball_basename)
+
+    # [TODO]: should it be limited to only certain files? maybe use
+    # tarfile.TarFile.add() to individually add files.
+    logger.info("creating level 1 tarball...")
+    try:
+        tarball_filename = make_tarball(
+            tarball_filename,
+            process_basedir,
+            os.path.join(run.observing_day, "level1"),
+        )
+    except Exception as e:
+        logger.error(f"error creating level 1 tarball: {e}")
+        return
     logger.info("created level 1 tarball")
-    # [TODO]: put link to tarball in gateway directory
-    logger.info("sent level 1 tarball to archive via gateway")
+
+    tarlist_basename = f"{run.observing_day}.chromag.l1.{__version__}.tarlist"
+    tarlist_filename = os.path.join(l1_dir, tarlist_basename)
+    make_tarlist(tarball_filename, tarlist_filename)
+    logger.info("created level 1 tarlist")
+
+    gateway_dir = get_option("archive", "gateway_dir")
+    if gateway_dir is not None:
+        if not os.path.isdir(gateway_dir):
+            create_dir(gateway_dir)
+
+        gateway_filename = os.path.join(gateway_dir, tarball_basename)
+        if os.path.islink(gateway_filename):
+            os.remove(gateway_filename)
+
+        os.symlink(tarball_filename, gateway_filename)
+        logger.info("sent level 1 tarball to archive via gateway")
