eed6cbb9cb523ebb2ff8b33b17f6abd0edd4f90b
Michael Galloy <mgalloy@gmail.com>
2026-08-22 07:58:48 -0600

    Add more fields to levels 0 and 1 database tables and insert files

--- chromag/database/insert.py
+++ chromag/database/insert.py
@@ -4,12 +4,15 @@
 """
 
 from contextlib import closing
+import os
 
 import mysql.connector
 
 from . import DatabaseError, get_connection, get_obsday_id, get_sw_id
 
 from ..config import get_option
+from ..datetime import datetime2dateobs
+from ..lines import available_lines
 from ..logging import logger
 
 
@@ -19,9 +22,29 @@ def insert_level0(
     catalog,
 ):
     """Update the chromag_level0 database table."""
-    logger.info("inserting level 0 files into database...")
+    logger.info(f"inserting {len(catalog)} level 0 files into database...")
     with closing(connection.cursor()) as cursor:
-        pass
+        for f in catalog:
+            fields = {
+                "filename": (f'"{f.basename}"', "s"),
+                "filesize": (os.path.getsize(f.filename), "d"),
+                "date_obs": (f'"{datetime2dateobs(f.date_obs)}"', "s"),
+                "obsday_id": (obsday_id, "d"),
+                "datatype": (f'"{f.datatype}"', "s"),
+                "object": (f'"{f.object}"', "s"),
+                "wave_region": (f'"{f.wave_region}"', "s"),
+                "wavelength": (f.wavelength, "0.3f"),
+                "exposure": (f.exposure, "0.3f"),
+                "scan_i": (f.scan_i, "d"),
+                "scan_n": (f.scan_n, "d"),
+            }
+            field_names = ",".join(fields.keys())
+            field_values = ",".join([f"{v[0]:{v[1]}}" for v in fields.values()])
+            cmd = f"insert into chromag_level0 ({field_names}) value ({field_values});"
+            cursor.execute(cmd)
+            logger.debug(f"inserted {f.basename}")
+
+    connection.commit()
 
 
 def insert_level1(
@@ -31,9 +54,36 @@ def insert_level1(
 ):
     """Update the chromag_level1 database table."""
     logger.info("inserting level 1 files into database...")
+
     sw_id = get_sw_id(connection)
-    logger.info("not implemented")
-    logger.warning("not implemented")
+    wave_regions = available_lines()
+    with closing(connection.cursor()) as cursor:
+        for w in wave_regions:
+            for f in catalog[catalog.is_science & (catalog.wave_region == w)]:
+                l1_file = f.l1_file
+                filename = l1_file.get_filename("filename")
+                basename = os.path.basename(filename)
+                fields = {
+                    "filename": (f'"{basename}"', "s"),
+                    "l0_filename": (f'"{f.basename}"', "s"),
+                    "filesize": (os.path.getsize(filename), "d"),
+                    "date_obs": (f'"{datetime2dateobs(f.date_obs)}"', "s"),
+                    "obsday_id": (obsday_id, "d"),
+                    "wave_region": (f'"{f.wave_region}"', "s"),
+                    "wavelength": (f.wavelength, "0.3f"),
+                    "exposure": (f.exposure, "0.3f"),
+                    "scan_i": (f.scan_i, "d"),
+                    "scan_n": (f.scan_n, "d"),
+                    "chromag_sw_id": (sw_id, "d"),
+                }
+                field_names = ",".join(fields.keys())
+                field_values = ",".join([f"{v[0]:{v[1]}}" for v in fields.values()])
+                cmd = f"insert into chromag_level1 ({field_names}) value ({field_values});"
+                cursor.execute(cmd)
+
+                logger.debug(f"inserted {basename}")
+
+    connection.commit()
 
 
 def insert_level2(
