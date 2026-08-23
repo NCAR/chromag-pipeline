114532e942c27d488bcd2328ffe6bf2df8822158
Michael Galloy <mgalloy@gmail.com>
2026-08-23 13:39:04 -0600

    Timeline plot updates
    - Use "stepfilled" histtype
    - Documentation

--- chromag/plot/timeline.py
+++ chromag/plot/timeline.py
@@ -11,6 +11,7 @@ from ..lines import available_lines, line_property
 
 START_TIME = 6  # 6 am HST
 END_TIME = 18  # 6 pm HST
+BINS_PER_HOUR = 6  # bins are 10 minutes
 
 
 def obsday_hours_formatter(obsday_hours: float, pos: float) -> str:
@@ -20,11 +21,22 @@ def obsday_hours_formatter(obsday_hours: float, pos: float) -> str:
     return obsday_hours2str(obsday_hours)
 
 
+def darken(color: str, factor: float = 0.5) -> str:
+    """Darken the red, blue, green components of a color by the given factor.
+    The color must be specified as "#RRGGBB".
+    """
+    r = max([int(factor * int(color[1:3], 16)), 0])
+    g = max([int(factor * int(color[3:5], 16)), 0])
+    b = max([int(factor * int(color[5:7], 16)), 0])
+    return f"#{r:02x}{g:02x}{b:02x}"
+
+
 def write_timeline(output_filename: str, catalog, binsize: int = 15):
     """Create a timeline of the observations for the day."""
     wave_regions = available_lines()
     figsize = (7, 2)
     label_fontsize = 8
+    edge_darkening_factor = 0.7
     n_rows = len(wave_regions) + 1  # one for every wave region plus darks
     n_cols = 1
     fig, axes = plt.subplots(
@@ -35,11 +47,18 @@ def write_timeline(output_filename: str, catalog, binsize: int = 15):
         wave_files = catalog[catalog.wave_region == w]
         wave_color = line_property(w, "color")
 
+        # [TODO]: to add flats/cal files:
+        # - change histtype to "stepfilled"
+        # - pass [sci_files, flat_files, cal_files]
+        # - pass color=[sci_color, flat_color, cal_color]
+        # - use alpha=
         axes[i].hist(
             wave_files.obsday_hours,
-            bins=(END_TIME - START_TIME) * 10,
+            bins=(END_TIME - START_TIME) * BINS_PER_HOUR,
             range=(START_TIME, END_TIME),
             color=wave_color,
+            edgecolor=darken(wave_color, factor=edge_darkening_factor),
+            histtype="edgefilled",
         )
         axes[i].set_ylim(0, 30)
         axes[i].tick_params(
@@ -55,9 +74,11 @@ def write_timeline(output_filename: str, catalog, binsize: int = 15):
     dark_index = len(wave_regions)
     axes[dark_index].hist(
         dark_files.obsday_hours,
-        bins=(END_TIME - START_TIME) * 10,
+        bins=(END_TIME - START_TIME) * BINS_PER_HOUR,
         range=(START_TIME, END_TIME),
         color="#606060",
+        edgecolor=darken("#606060", factor=edge_darkening_factor),
+        histtype="edgefilled",
     )
     axes[dark_index].set_ylim(0, 15)
     axes[dark_index].set_yticks([])
