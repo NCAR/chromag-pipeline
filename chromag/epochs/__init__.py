15b4fa6363d2ff7a2dd2078dc2a9896960c9e37b
Michael Galloy <mgalloy@gmail.com>
2026-08-27 13:24:37 -0600

    Only read epochs file when needed

--- chromag/epochs/__init__.py
+++ chromag/epochs/__init__.py
@@ -10,15 +10,21 @@ import epochs
 
 DateValue = TypeVar("DateValue", str, datetime.datetime)
 
-EPOCHS_ROOT = os.path.dirname(os.path.abspath(__file__))
-EPOCHS_CFG = os.path.join(EPOCHS_ROOT, "epochs.cfg")
-EPOCHS_SPEC = os.path.join(EPOCHS_ROOT, "epochs.spec.cfg")
 
-
-ep = epochs.EpochConfigParser(EPOCHS_SPEC)
-ep.read(EPOCHS_CFG)
+ep = None
 
 
 def get(property_name, date: DateValue):
     """Get property value for a given datetime."""
+    global ep
+
+    # read the epochs files if it hasn't been initialized already
+    if ep is None:
+        epochs_root = os.path.dirname(os.path.abspath(__file__))
+        epochs_spec_filename = os.path.join(epochs_root, "epochs.spec.cfg")
+        ep = epochs.EpochConfigParser(epochs_spec_filename)
+
+        epochs_cfg_filename = os.path.join(epochs_root, "epochs.cfg")
+        ep.read(epochs_cfg_filename)
+
     return ep.get(property_name, date)
