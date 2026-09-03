145c5e4f313eda115ff91c0b0f5fb74d8d5d6001
Michael Galloy <mgalloy@gmail.com>
2026-09-02 20:46:46 -0600

    Add a query method to query module

--- chromag/database/query.py
+++ chromag/database/query.py
@@ -3,6 +3,18 @@
 """Utilities for querying the database.
 """
 
+from contextlib import closing
+
 import mysql.connector
 
 from ..logging import logger
+
+
+def query(
+    connection: mysql.connector.connection_cext.CMySQLConnection,
+    sql_cmd: str,
+):
+    with closing(connection.cursor()) as cursor:
+        cursor.execute(sql_cmd)
+        results = cursor.fetchall()
+    return results
