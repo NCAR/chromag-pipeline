ff01bd5103f2534f0d4e27fa34125461eca872fc
Michael Galloy <mgalloy@gmail.com>
2026-09-11 12:54:29 -0600

    Add some header comments and simplify logic a bit

--- chromag/control.py
+++ chromag/control.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """Module handling control of whether (re)processing of dates is stopped.
+
+There is just a single file, `command.txt`, in `process.controldir` that
+contains either "RUNNING" or "STOPPED" that indicates the processing state
+of the pipeline. If the file contains, anything besides "RUNNING", the pipeline
+is stopped and new runs of the pipeline will not start (existing runs will
+finish).
 """
 
 import os
@@ -9,6 +15,10 @@ from .config import get_option
 
 
 def _get_controlfile() -> str | None:
+    """Returns the fullpath to the "command.txt" file that stores the
+    (re)processing state. If `process.controldir` is not set, returns `None`.
+    This routine will create the `controldir` if `process.controldir` is set,
+    but hasn't been created yet."""
     controldir = get_option("process", "controldir")
     if controldir is not None:
         if not os.path.isdir(controldir):
@@ -20,6 +30,8 @@ def _get_controlfile() -> str | None:
 
 
 def is_running():
+    """Determine whether the (re)processing is running or stopped. If
+    `process.controldir` is not set, returns `True`."""
     controlfile = _get_controlfile()
     if controlfile is not None:
         with open(controlfile, "r") as f:
@@ -29,23 +41,32 @@ def is_running():
         return True
 
 
-def _set_controlfile(value: str):
+def _set_controlfile(value: str) -> bool:
+    """Helper routine to set the value of the (re)processing state of the
+    pipeline. `value` must be either "RUNNING" or "STOPPED". If
+    `process.controldir` is not set, returns `False`."""
     controlfile = _get_controlfile()
     if controlfile is not None:
+        with open(controlfile, "r") as f:
+            old_value = f.read()
+        if (old_value == "RUNNING") == (value == "RUNNING"):
+            return False
         with open(controlfile, "w") as f:
             f.write(value)
-            return True
+        return True
     else:
         return False
 
 
-def set_running():
-    if is_running():
-        return False
+def set_running() -> bool:
+    """Set the (re)processing state to be running, i.e., new runs are allowed.
+    Returns a boolean indicating whether calling this routine changed anything.
+    If `process.controldir` is not set, returns `False`."""
     return _set_controlfile("RUNNING")
 
 
-def set_stopped():
-    if not is_running():
-        return False
+def set_stopped() -> bool:
+    """Set the (re)processing state to be stopped, i.e., new runs are not
+    allowed. Returns a boolean indicating whether calling this routine changed
+    anything. If `process.controldir` is not set, returns `False`."""
     return _set_controlfile("STOPPED")
